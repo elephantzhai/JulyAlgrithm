1748bb35855630621ecfa539e9679282b2113f5c
elephantzhai <elephantzhai@gmail.com>
2014-01-09 21:58:09 +0800

    chapter02 sortCompareString

--- JulyAlgrithm/JulyAlgrithm/JulyAlgrithm/chapter02/CompareString.cpp
+++ JulyAlgrithm/JulyAlgrithm/JulyAlgrithm/chapter02/CompareString.cpp
@@ -2,8 +2,62 @@
 #include "CompareString.h"
 
 void compareStringMain(){
-	volientCompareStringSample();
+	//volientCompareStringSample();
+	sortCompareStringSample();
 
+
+}
+void sortCompareStringSample(){
+	string a = "abcdefg";
+	string b = "abc";
+	quickSort(a,0,a.length()-1);
+	quickSort(b,0,b.length()-1);
+	bool result =sortCompareString(a,b);
+	if(result)
+		cout<<"true"<<endl;
+	else
+		cout<<"false"<<endl;
+
+}
+
+void quickSort(string &str,int lo,int hi){
+	if(lo<hi){
+		int mid = partition(str,lo,hi);
+		quickSort(str,lo,mid-1);
+		quickSort(str,mid+1,hi);
+	}
+}
+
+int partition(string &str,int lo,int hi){
+	char key = str[hi];
+	int i = lo-1;
+
+	for(int j=0;j<hi;j++){
+		if(str[j]<=key){
+			i++;
+			swap(str[i],str[j]);
+		}
+	}
+	swap(str[hi],str[i+1]);
+	return i+1;
+}
+
+bool sortCompareString(string &longStr,string &shortStr){
+	int strLongPostion = 0;
+	int strShortPostion = 0;
+	bool isContain = true;
+	while(strLongPostion<longStr.length() && strShortPostion<shortStr.length()){
+		while(longStr[strLongPostion]<shortStr[strShortPostion] && strLongPostion<longStr.length()-1)
+			strLongPostion++;
+		if(longStr[strLongPostion]!=shortStr[strShortPostion]){
+			isContain = false;
+			break;
+		}else{
+			strLongPostion++;
+			strShortPostion++;
+		}
+	}
+	return isContain;
 }
 
 void volientCompareStringSample(){
--- JulyAlgrithm/JulyAlgrithm/JulyAlgrithm/chapter02/CompareString.h
+++ JulyAlgrithm/JulyAlgrithm/JulyAlgrithm/chapter02/CompareString.h
@@ -4,5 +4,10 @@ using namespace std;
 
 void compareStringMain();
 
+void sortCompareStringSample();
+void quickSort(string &str,int lo,int hi);
+int partition(string &str,int lo,int hi);
+bool sortCompareString(string &longStr,string &shortStr);
+
 void volientCompareStringSample();
 bool volientCompareString(string &a,string &b);
