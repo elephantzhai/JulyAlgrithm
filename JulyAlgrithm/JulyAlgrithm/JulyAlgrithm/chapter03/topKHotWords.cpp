c1f07142aabe387a8e917cd9a216594320a2b564
elephantzhai <elephantzhai@gmail.com>
2014-01-22 23:18:07 +0800

    chapter03 TopKHotWords

--- JulyAlgrithm/JulyAlgrithm/JulyAlgrithm/chapter03/topKHotWords.cpp
+++ JulyAlgrithm/JulyAlgrithm/JulyAlgrithm/chapter03/topKHotWords.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 #include <string>
 #include <assert.h>
+#include <string.h>
+#include <stdio.h>
 using namespace std;
 
 #define HASHLEN 2807303
-#define WORDLEN 30;
+#define WORDLEN 30
 
 typedef struct node_no_space *ptr_no_space;
-typedef struct node_has-space *ptr_has_space;
+typedef struct node_has_space *ptr_has_space;
 ptr_no_space head[HASHLEN];
 
-struct node_no_space{
+struct node_no_space
+{
     char *word;
     int count;
     ptr_no_space next;
 };
 
-struct node_has_space{
+struct node_has_space
+{
     char word[WORDLEN];
     int count;
     ptr_has_space next;
@@ -36,7 +40,7 @@ int hash_function(char const *p){
 void append_word(char const *str){
     int index = hash_function(str);
     ptr_no_space p = head[index];
-    while(p != null){
+    while(p != NULL){
         if(strcmp(str,p->word)==0){
             (p->count)++;
             return;
@@ -54,15 +58,43 @@ void append_word(char const *str){
 }
 
 void write_to_file(){
-    File *fp = fopen("reaulst.txt","w");
+    FILE *fp = fopen("reaulst.txt","w");
     assert(fp);
 
     int i= 0;
     while(i < HASHLEN){
-        for(ptr_no_space p = head[i];p != null;p=p->next)
+        for(ptr_no_space p = head[i];p != NULL;p=p->next)
             fprintf(fp, "%s %d\n", p->word,p->count);
         i++;
     }
     fclose(fp);
+}
 
+void sift_down(node_has_space heap[],int i,int len){
+    int min_index = -1;
+    int left = 2*i;
+    int right = 2*i+1;
+
+    if(left<=len && heap[left].count<heap[i].count)
+        min_index = left;
+    else
+        min_index = i;
+
+    if(right<=len && heap[right].count<heap[min_index].count)
+        min_index = right;
+    if(min_index != i){
+        swap(heap[i].count,heap[min_index].count);
+
+        char buffer[WORDLEN];
+        strcpy(buffer,heap[i].word);
+        strcpy(heap[i].word,heap[min_index].word);
+        strcpy(heap[min_index].word,buffer);
+
+        sift_down(heap,min_index,len);
+    }
+
+}
+
+int main(){
+    return 0;
 }
--- JulyAlgrithm/testCan2013/testCan2013/test.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-	cout<<"hello , just a test";
-	int i;
-	cin>> i;
-}
