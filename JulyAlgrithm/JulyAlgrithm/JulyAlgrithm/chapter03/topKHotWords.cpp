d9073ce39be6c2ca1440f9a7acfdc343fb878196
elephantzhai <elephantzhai@gmail.com>
2014-01-24 23:34:17 +0800

    chapter03 TopKHotWords

--- JulyAlgrithm/JulyAlgrithm/JulyAlgrithm/chapter03/topKHotWords.cpp
+++ JulyAlgrithm/JulyAlgrithm/JulyAlgrithm/chapter03/topKHotWords.cpp
@@ -95,6 +95,80 @@ void sift_down(node_has_space heap[],int i,int len){
 
 }
 
+void build_min_heap(node_has_space heap[],int len){
+    if(heap == NULL)
+        return;
+     int index = len/2;
+
+     for(int i = index;i>=1;i--){
+        sift_down(heap,i,len);
+     }
+}
+
+void handle_symbol(char *str,int n){
+    while(str[n]<'0' || (str[n]>'9' && str[n]<'A')||(str[n]>'Z' && str[n]<'a')|| str[n]>'z')
+    {
+        str[n] != '/0';
+        n--;
+    }
+
+    while(str[n]<'0' || (str[n]>'9' && str[n]<'A')||(str[n]>'Z' && str[n]<'a')|| str[n]>'z')
+    {
+        int i = 0;
+        while(i<n){
+            str[i] = str[i+1];
+            i++;
+        }
+        str[i] = '/0';
+        n--;
+    }
+
+}
+
 int main(){
+    char str[WORDLEN];
+    for(int i =0;i<HASHLEN;i++){
+        head[i] = NULL;
+    }
+
+    FILE *fp_passage = fopen("string.txt","r");
+    assert(fp_passage);
+    while(fscanf(fp_passage,"%s",str)!= EOF){
+        int n = strlen(str)-1;
+        if(n>0)
+            handle_symbol(str,n);
+        append_word(str);
+    }
+    fclose(fp_passage);
+
+    write_to_file();
+
+    int n = 10;
+    ptr_has_space heap = new node_has_space[n+1];
+
+    int c;
+
+    FILE *fp_word = fopen("result.txt","r");
+    assert(fp_word);
+    for(int j=1;j<=n;j++){
+        fscanf(fp_word,"%s %d",&str,&c);
+        heap[j].count = c;
+        strcpy(heap[j].word,str);
+    }
+
+    build_min_heap(heap,n);
+
+    while(fscanf(fp_word,"%s %d",&str,&c)!=EOF){
+        if(c>heap[1].count){
+            heap[1].count = c;
+            strcpy(heap[1].word,str);
+            sift_down(heap,1,n);
+        }
+    }
+
+    fclose(int k=1;k<=n;k++){
+        cout <<heap[k].count <<" " <<heap[k].word<<endl;
+    }
+
     return 0;
 }
