dc84aefc704f559fcb1feaa6b203c893fe7e94bc
elephantzhai <elephantzhai@gmail.com>
2014-02-27 22:54:03 +0800

    chapter05 SmallToBigToSmall

--- JulyAlgrithm/JulyAlgrithm/JulyAlgrithm/chapter05/MoreSum.cpp
+++ JulyAlgrithm/JulyAlgrithm/JulyAlgrithm/chapter05/MoreSum.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <list>
+#include <memory.h>
+#include <stdlib.h>
 using namespace std;
 
 list<int> list1;
@@ -21,10 +23,53 @@ void find_facetor(int sum,int n){
 	find_facetor(sum,n-1);
 }
 
+void sumofsub(int t,int k,int r,int &M ,bool &flag,bool *X)
+{
+	X[k] = true;
+	if(t+k == M){
+		flag = true;
+		for(int i=1;i<=k;++i){
+			if(X[i]==1){
+				cout<<i<<" ";
+			}
+		}
+		cout<<endl;
+	}
+	else{
+		if(t+k+(k+1)<=M){
+			sumofsub(t+k,k+1,r-k,M,flag,X);
+		}
+
+		if((t+r-k>=M)&&(t+(k+1)<=M)){
+			X[k] = false;
+			sumofsub(t,k+1,r-k,M,flag,X);
+		}
+	}
+}
+
+void search(int &N,int &M)
+{
+	bool* X = (bool*)malloc(sizeof(bool)*(N+1));
+	memset(X,false,sizeof(bool)*(N+1));
+	int sum = (N+1)*N*0.5f;
+	if(1>M || sum < M)
+	{
+		cout<<"not found"<<endl;
+		return;
+	}
+	bool f = false;
+	sumofsub(0,1,sum,M,f,X);
+	if(!f){
+		cout<<"not found"<<endl;
+	}
+	free(X);
+}
+
 int main(){
 	int sum,n;
 	sum = 6;
 	n = 3;
 	find_facetor(sum,n);
+	search(n,sum);
 	return 0;
 }
--- JulyAlgrithm/JulyAlgrithm/JulyAlgrithm/chapter05/SmallToBigToSmall.cpp
+++ JulyAlgrithm/JulyAlgrithm/JulyAlgrithm/chapter05/SmallToBigToSmall.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
-#define MAX_NUM (1U<<31)
+#include <stdio.h>
+#define MAX_NUM (1U<<30)
 
 using namespace std;
-int array[] = {100,99,98,1,2,3};
+
 int main(){
 	int i,n,low,high,mid,max;
 	n = 6;
-	double a[n],b[n],c[n],inc[n];
+	int a[n],b[n],c[n],inc[n];
+	int array[] = {1,4,2,5,3,6};
 	for(int i=0;i<n;i++){
 		a[i] = array[i];
 	}
 
-	for(i=0;i<n;i++)
-		inc[i] = (unsigned)MAX_NUM;
-
 	int j;
 
+	for(int i=0;i<n;i++){
+		b[i] = 0;
+		inc[i] = (unsigned) MAX_NUM;
+	}
 
-	for(i=0;i<n;i++){
+	for(int i=0;i<n;i++){
 		low = 0;high = i;
 		while(low<high){
 			mid = low +(high-low)*0.5;
@@ -27,13 +30,29 @@ int main(){
 		b[i] = low+1;
 		inc[low] = a[i];
 
-		cout<<"i:"<<i<<"low:"<<low<<"high:"<<high<<endl;
-		for(j=0;j<n;j++)cout<<b[i]<<" ";
-			cout<<endl;
-		for(j=0;j<n;j++)cout<<inc[i]<<" ";
-			cout<<endl;
+	}
+
+	for(int i=0;i<n;i++){
+		c[i] = 0;
+		inc[i] = (unsigned) MAX_NUM;
+	}
+
+	for(int i=n-1;i>=0;i--){
+		low = 0;high = i;
+		while(low<high){
+			mid = low +(high-low)*0.5;
+			if(inc[mid]<a[i])low=mid+1;
+			else high = mid;
+		}
+		c[i] = low+1;
+		inc[low] = a[i];
 
 	}
+
+	max = 0;
+	for(i = 0;i<n;i++)
+		if (b[i]+c[i]>max) {max = b[i]+c[i];j=i;}
+	cout<<n+1-max<<endl;
 
 
 	return 0;
