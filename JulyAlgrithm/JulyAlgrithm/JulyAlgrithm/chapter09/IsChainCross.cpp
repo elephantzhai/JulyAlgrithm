153a0f6803729485972be647d379cfd5d6880eb3
elephantzhai <elephantzhai@gmail.com>
2014-03-21 00:40:07 +0800

    chapter09 FindFirstCommonNode

--- JulyAlgrithm/JulyAlgrithm/JulyAlgrithm/chapter09/IsChainCross.cpp
+++ JulyAlgrithm/JulyAlgrithm/JulyAlgrithm/chapter09/IsChainCross.cpp
@@ -61,3 +61,42 @@ bool detect(Node *head1,Node *head2){
 	}
 
 }
+
+unsigned int ListLength(ListNode* pHead){
+	unsighed int nLength = 0;
+	ListNode* pNode = pHead;
+	while(pNode != NULL){
+		++ nLength;
+		pNode = pNode -> next;
+	}
+	return nLength;
+}
+
+ListNode* FindFirstCommonNode(ListNode *pHead1,ListNode *pHead2){
+	unsigned int nLength1 = ListLength(pHead1);
+	unsigned int nLength2 = ListLength(pHead2);
+	int nLengthDif = nLength1 - nLength2;
+
+	ListNode *pListHeadLong = pHead1;
+	ListNode *pListHeadShort = pHead2;
+	if(nLength2 > nLength1){
+		pListHeadLong = pHead2;
+		pListHeadShort = pHead1;
+		nLengthDif = nLength2 - nLength1;
+	}
+
+	for(int i=0;i<nLengthDif;++i){
+		pListHeadLong = pListHeadLong->next;
+	}
+
+	while((pListHeadLong!=NULL) && (pListHeadShort!=NULL) && (pListHeadShort!=pListHeadLong){
+		pListHeadLong = pListHeadLong->next;
+		pListHeadShort = pListHeadShort->next;
+	}
+
+	ListNode *pFirstCommonNode = NULL;
+	if(pListHeadLong == pListHeadShort)
+		pFirstCommonNode = pListHeadLong;
+
+	return pFirstCommonNode;
+}
