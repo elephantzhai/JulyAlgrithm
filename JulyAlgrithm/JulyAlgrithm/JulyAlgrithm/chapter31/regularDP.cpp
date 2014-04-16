c9f713de7a035a81ec38c8512461bddae4f3a2f9
elephantzhai <elephantzhai@gmail.com>
2014-04-17 00:54:16 +0800

    chapter31 regualrDP

--- JulyAlgrithm/JulyAlgrithm/JulyAlgrithm/chapter31/regularDP.cpp
+++ JulyAlgrithm/JulyAlgrithm/JulyAlgrithm/chapter31/regularDP.cpp
@@ -31,7 +31,7 @@ char* my_find(char input[],char rule[]){
 				}
 			}else if(rule[j-1]=='?'){
 				if(dp[i-1][j-1]!=-1){
-					dp[i][j] = dp[i][j]+1;
+					dp[i][j] = dp[i-1][j-1]+1;
 				}else{
 					dp[i][j] = -1;
 				}
@@ -43,19 +43,19 @@ char* my_find(char input[],char rule[]){
 		}
 	}
 
-	for(int i=0;i<=len1;i++){
-		for(int j=0;j<=len2;j++){
-			cout<<dp[i][j]<<" ";
-		}
-		cout<<endl;
-	}
+	// for(int i=0;i<=len1;i++){
+	// 	for(int j=0;j<=len2;j++){
+	// 		cout<<dp[i][j]<<" ";
+	// 	}
+	// 	cout<<endl;
+	// }
 
 	int m = -1;
 	int *ans = new int[len1];
 	int count_ans = 0;
 	char *returnans = new char[len1+1];
 	int count = 0;
-	for(int i=1;i<len1;i++){
+	for(int i=1;i<=len1;i++)
 		if(dp[i][len2]>m){
 			m = dp[i][len2];
 			count_ans = 0;
@@ -64,21 +64,22 @@ char* my_find(char input[],char rule[]){
 			ans[count_ans++] = i-m;
 		}
 
-		if(count_ans!=0){
-			int len = ans[0];
-			for(int i = 0;i<m;i++){
-				returnans[count++] = input[i+ans[0]];
-			}
-			for(int j =1;j<count_ans;j++){
-				returnans[count++] = ' ';
-				len = ans[j];
-				for(int i =0;i<m;i++){
-					returnans[count++] = input[i+ans[j]];
-				}
+	if(count_ans!=0){
+		int len = ans[0];
+		for(int i = 0;i<m;i++){
+			cout<<input[i+ans[0]];
+			returnans[count++] = input[i+ans[0]];
+		}
+		for(int j =1;j<count_ans;j++){
+			returnans[count++] = ' ';
+			len = ans[j];
+			for(int i =0;i<m;i++){
+				returnans[count++] = input[i+ans[j]];
 			}
 		}
-	
 		
+	
+		cout<<endl;
 		returnans[count++] = '\0';
 
 }
@@ -88,8 +89,8 @@ char* my_find(char input[],char rule[]){
 }
 
 int main(){
-	char input[] = "aabcde";
-	char rule[] = "a*d";
+	char input[] = "acddagd";
+	char rule[] = "a?d";
 	cout<<my_find(input,rule)<<endl;
 	return 0;
 }
